d0dd78e0e8c84dc240e8a52659d7970984fa5406
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-14 08:41:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let tinggiBadan = [169, 180, 154, 160, 173, 177, 152, 159, 178, 162];
+for (let i = 0; i < tinggiBadan.length; i++) {
+  console.log(tinggiBadan[i]);
+}
+console.log("");
+for (let t of tinggiBadan) {
+  console.log(t);
+}
